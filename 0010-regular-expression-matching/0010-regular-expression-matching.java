78aae8ac65bf6ffb624709d1b57cb60c1d6c75c0
Haitam Lahlaouti <147536244+iBOY011@users.noreply.github.com>
2024-12-30 22:09:00 +0100

    Time: 981 ms (5.14%), Space: 45.4 MB (5.33%) - LeetHub

---
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=10 lang=java
+ *
+ * [10] Regular Expression Matching
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isMatch(String s, String p) {
+
+        if (p.isEmpty()) {
+            return s.isEmpty();
+        }
+
+        boolean firstMatch = (!s.isEmpty() && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.'));
+
+        if (p.length() >= 2 && p.charAt(1) == '*') {
+            return (isMatch(s, p.substring(2)) || (firstMatch && isMatch(s.substring(1), p)));
+        } else {
+            return firstMatch && isMatch(s.substring(1), p.substring(1));
+        }
+        
+    }
+}
+// @lc code=end
+
