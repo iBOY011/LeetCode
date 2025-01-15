c8b5bb1c5836ae65fbd5f5a422604f1ddbb1c463
Haitam Lahlaouti <147536244+iBOY011@users.noreply.github.com>
2025-01-15 14:19:32 +0100

    Time: 0 ms (100%), Space: 41.8 MB (76.19%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=66 lang=java
+ *
+ * [66] Plus One
+ */
+
+// @lc code=start
+class Solution {
+    public int[] plusOne(int[] digits) {
+
+        for(int i = digits.length - 1; i >= 0; i--) {
+            if(digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        int[] result = new int[digits.length + 1];
+        result[0] = 1;
+
+        return result;
+        
+    }
+}
+// @lc code=end
+
