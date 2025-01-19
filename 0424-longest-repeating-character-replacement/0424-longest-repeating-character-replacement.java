fe8362878cc32a3b72d183b500d86525c4baf1d9
Haitam Lahlaouti <147536244+iBOY011@users.noreply.github.com>
2025-01-19 17:50:26 +0100

    Time: 9 ms (78.25%), Space: 44.3 MB (95.55%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=424 lang=java
+ *
+ * [424] Longest Repeating Character Replacement
+ */
+
+// @lc code=start
+class Solution {
+    public int characterReplacement(String s, int k) {
+
+        int[] count = new int[26];
+        int start = 0;
+        int maxCount = 0;
+        int maxLength = 0;
+
+        for (int end = 0; end < s.length(); end++) {
+            maxCount = Math.max(maxCount, ++count[s.charAt(end) - 'A']);
+            if (end - start + 1 - maxCount > k) {
+                count[s.charAt(start) - 'A']--;
+                start++;
+            }
+            maxLength = Math.max(maxLength, end - start + 1);
+        }
+
+        return maxLength;
+        
+    }
+}
+// @lc code=end
+
