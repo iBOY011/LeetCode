0d55bf85026f57a3c7c8faa3cd1c8c3a52f08a73
Haitam Lahlaouti <147536244+iBOY011@users.noreply.github.com>
2025-01-21 22:41:24 +0100

    Time: 0 ms (100%), Space: 41.2 MB (23.81%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=7 lang=java
+ *
+ * [7] Reverse Integer
+ */
+
+// @lc code=start
+class Solution {
+    public int reverse(int x) {
+
+        int result = 0;
+        while (x != 0) {
+            int tail = x % 10;
+            int newResult = result * 10 + tail;
+            if ((newResult - tail) / 10 != result) {
+                return 0;
+            }
+            result = newResult;
+            x = x / 10;
+        }
+
+        return result;
+        
+    }
+}
+// @lc code=end
+
