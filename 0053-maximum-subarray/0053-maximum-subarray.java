4e5cc62b3271458c29b105486526c27b1a102954
Haitam Lahlaouti <147536244+iBOY011@users.noreply.github.com>
2025-01-22 19:55:17 +0100

    Time: 1 ms (99.36%), Space: 56.9 MB (68.89%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=53 lang=java
+ *
+ * [53] Maximum Subarray
+ */
+
+// @lc code=start
+class Solution {
+    public int maxSubArray(int[] nums) {
+
+        int maxSum = nums[0];
+        int currentSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+        
+    }
+}
+// @lc code=end
+
